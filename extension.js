c9af66cf81fd664ef6e3e671f1440567e4524bb7
Daniel García Moreno <dani@danigm.net>
2023-10-13 13:11:22 +0200

    Migrate to GNOME 45

--- extension.js
+++ extension.js
@@ -1,48 +1,37 @@
-const St = imports.gi.St;
-const Main = imports.ui.main;
+import St from 'gi://St';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import { RunDialog } from 'resource:///org/gnome/shell/ui/runDialog.js';
 
-const RunDialog = imports.ui.runDialog;
+const basef = RunDialog.prototype._run;
 
-let basef = RunDialog._run;
 
-function init() {
-}
-
-function enable() {
-    if (Main.runDialog == null) {
-        Main.runDialog = new RunDialog.RunDialog();
+export default class Calc {
+    constructor() {
     }
-    let r = Main.runDialog;
-    basef = r._run.bind(r);
 
-    function calculator(input, inTerminal) {
-        this._commandError = false;
-        let value = "";
-        if (input.match(/^=([\d\+\-\*\/\(\) \.]+)$/)) {
-            try {
-                value = String(eval(input.substr(1)));
-            } catch (e) {
-                value = "error: " + String(e.message);
+    enable() {
+        RunDialog.prototype._run = function(input, inTerminal) {
+            this._commandError = false;
+            let value = "";
+            if (input.match(/^=([\d\+\-\*\/\(\) \.]+)$/)) {
+                try {
+                    value = String(eval(input.substr(1)));
+                } catch (e) {
+                    value = "error: " + String(e.message);
+                }
+                this._entryText.set_text('=' + value);
+                this._showError(value);
+                Main.notify(value, '');
+
+                let clipboard = St.Clipboard.get_default();
+                clipboard.set_text(St.ClipboardType.CLIPBOARD, value);
+            } else {
+                basef.bind(this)(input, inTerminal);
             }
-            this._entryText.set_text('=' + value);
-            this._showError(value);
-            Main.notify(value, '');
-
-            let clipboard = St.Clipboard.get_default();
-            clipboard.set_text(St.ClipboardType.CLIPBOARD, value);
-        } else {
-            basef(input, inTerminal);
-        }
+        };
     }
 
-    r._run = calculator.bind(r);
-}
-
-function disable() {
-    if (Main.runDialog == null) {
-        Main.runDialog = new RunDialog.RunDialog();
+    disable() {
+        RunDialog.prototype._run = basef;
     }
-    let r = Main.runDialog;
-
-    r._run = basef;
 }
